6ab4ceb26938bd7f0f4bc1fea1802337acf77b69
luqman9000 <luqmanhal900@gmail.com>
2023-04-10 01:24:20 +0800

    don't hurt me, no more

--- fuckthishit/Form1.Designer.cs
+++ fuckthishit/Form1.Designer.cs
@@ -34,19 +34,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(312, 233);
+            this.label1.Location = new System.Drawing.Point(416, 287);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.Size = new System.Drawing.Size(321, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "wwadwadwadwa";
+            this.label1.Text = "what is love, baby dont hurt me, dont hurt me, no more";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
